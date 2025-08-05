a5721d85a750fe08fcce4f201f16345439e08bd2
set <lucameopou@gmail.com>
2025-08-05 12:47:15 +0200

    feat: add "use client" directive for nextjs compatibility

--- packages/turnstile/src/turnstile.tsx
+++ packages/turnstile/src/turnstile.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { createCaptchaComponent } from "@react-captcha/core/src/base-captcha";
 import { TurnstileProvider } from "./index";
 import type { Turnstile as TurnstileType } from "./types";
