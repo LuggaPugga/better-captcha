0bbe21cab61caa446477fc740826ecf25d88c361
Luca <lucameopou@gmail.com>
2025-10-11 12:24:52 +0200

    fix: simplify widgetId usage in TurnstileProvider reset method

--- packages/react-captcha/src/provider/turnstile/provider.ts
+++ packages/react-captcha/src/provider/turnstile/provider.ts
@@ -58,7 +58,7 @@ export class TurnstileProvider extends Provider<
 	}
 
 	reset(widgetId: string) {
-		window.turnstile.reset(`react-captcha-${widgetId}`);
+		window.turnstile.reset(widgetId);
 	}
 
 	async execute(widgetId: string) {
