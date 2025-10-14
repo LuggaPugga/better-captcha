c2a314ff2bc5876f2fe809d1fc1a01239e6228e8
Luca <lucameopou@gmail.com>
2025-10-15 00:05:56 +0200

    refactor: clean up ProsopoProvider by removing unused element property and simplifying render logic

--- packages/core/src/providers/prosopo/provider.ts
+++ packages/core/src/providers/prosopo/provider.ts
@@ -1,7 +1,7 @@
 import { type CaptchaHandle, Provider, type ProviderConfig } from "../../provider";
 import { loadScript } from "../../utils/load-script";
 import { getSystemTheme } from "../../utils/theme";
-import type { RenderParameters, RenderWidgetFunction, WidgetApi } from "./types";
+import type { RenderParameters, WidgetApi } from "./types";
 
 declare global {
 	interface Window {
@@ -13,7 +13,6 @@ export type ProsopoHandle = CaptchaHandle;
 
 export class ProsopoProvider extends Provider<ProviderConfig, Omit<RenderParameters, "siteKey">, ProsopoHandle> {
 	private widgetId: string | null = null;
-	private element: HTMLElement | null = null;
 
 	constructor(sitekey: string) {
 		super(
@@ -33,24 +32,16 @@ export class ProsopoProvider extends Provider<ProviderConfig, Omit<RenderParamet
 	}
 
 	render(element: HTMLElement, options?: Omit<RenderParameters, "siteKey">): string {
-		this.element = element;
 		this.widgetId = `prosopo-widget-${Date.now()}-${Math.random().toString(36).substr(2, 9)}`;
 
-		element.setAttribute("id", this.widgetId);
+		if (options?.theme === "auto") {
+			options.theme = getSystemTheme();
+		}
 
-		const renderOptions: RenderParameters = {
+		window.procaptcha?.render(element, {
 			siteKey: this.sitekey,
 			...options,
-		};
-
-		if (renderOptions.theme === "auto") {
-			renderOptions.theme = getSystemTheme();
-		}
-
-		if (window.procaptcha) {
-			const renderFunction: RenderWidgetFunction = window.procaptcha.render;
-			renderFunction(element, renderOptions);
-		}
+		});
 
 		return this.widgetId;
 	}
@@ -68,18 +59,6 @@ export class ProsopoProvider extends Provider<ProviderConfig, Omit<RenderParamet
 	}
 
 	destroy(_widgetId: string): void {
-		if (this.element) {
-			this.element.classList.remove("procaptcha");
-			this.element.removeAttribute("data-sitekey");
-			this.element.removeAttribute("data-captcha-type");
-			this.element.removeAttribute("data-theme");
-			this.element.removeAttribute("data-language");
-			this.element.removeAttribute("data-web3");
-			this.element.removeAttribute("data-callback");
-			this.element.removeAttribute("data-size");
-			this.element.removeAttribute("id");
-		}
-
 		if (this.widgetId) {
 			const callbackName = `prosopoCallback_${this.widgetId}`;
 			if ((window as Record<string, unknown>)[callbackName]) {
@@ -88,7 +67,6 @@ export class ProsopoProvider extends Provider<ProviderConfig, Omit<RenderParamet
 		}
 
 		this.widgetId = null;
-		this.element = null;
 	}
 
 	getResponse(_widgetId: string): string {
