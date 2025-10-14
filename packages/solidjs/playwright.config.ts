0e5141575289fe2c3dcc4035302e6fe2acc1dccc
Luca <lucameopou@gmail.com>
2025-10-14 17:25:07 +0200

    chore: update Playwright config command to point to the correct solidjs directory

--- packages/solidjs/playwright.config.ts
+++ packages/solidjs/playwright.config.ts
@@ -2,5 +2,5 @@ import { createPlaywrightConfig } from "@better-captcha/tests/playwright-config"
 
 export default createPlaywrightConfig({
 	baseURL: "http://localhost:9001",
-	command: "cd ../../apps/testing/solid && bun run dev",
+	command: "cd ../../apps/testing/solidjs && bun run dev",
 });
